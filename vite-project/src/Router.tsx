c6ad219b3dc308119d029a61b7fc6d93206b6f14
Rafael-M-Silva <rafael_mauricio@hotmail.com>
2022-10-14 01:38:09 -0300

    Roteamento com REACT

---
+++ vite-project/src/Router.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+
+import { Cart } from "./pages/cart";
+import { Catalog } from "./pages/Catalog";
+
+export function AppRoutes() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/catalog" element={<Catalog />} />
+            </Routes>
+        </Router>
+    )
+}
